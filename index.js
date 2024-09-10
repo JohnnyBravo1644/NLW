7c5eeb0541481fa3b731b3111c5c1d1086b25bdf
JohnnyBravo1644 <joavitorjuv@outlook.com>
2024-09-10 19:33:42 -0300

    realizadas algumas tarefas

---
+++ index.js
@@ -0,0 +1,26 @@
+//hello word
+let mensagem = "Hello World" 
+
+console.log (mensagem);
+
+//array
+let metas = ["omelander kill me wife and took me bloody son", "oi"]
+
+console.log(metas[1] + ", " + metas[0]);
+
+//objeto
+let metaObjeto = {
+    value: "valor aleatório",
+    checked: false,
+    log: (info) => {
+        console.log(info)
+    }
+}
+
+//metaObjeto.value = "How did you the eletric will do job?"
+metaObjeto.log (metaObjeto.value)
+
+//function // arrow function
+const criarMeta = () => {
+
+}

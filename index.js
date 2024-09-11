a050319591175d36af35ac314ebce761224f3e4f
JohnnyBravo1644 <joavitorjuv@outlook.com>
2024-09-11 18:11:05 -0300

    switch case

--- index.js
+++ index.js
@@ -1,34 +1,21 @@
-//hello word
-let mensagem = "Hello World" 
-
-console.log (mensagem);
-
-//array
-let metas = ["omelander kill me wife and took me bloody son", "oi"]
-
-console.log(metas[1] + ", " + metas[0]);
-
-//objeto
-let metaObjeto = {
-    value: "valor aleatório",
-    checked: false,
-    log: (info) => {
-        console.log(info)
-    }
-}
-
-//metaObjeto.value = "How did you the eletric will do job?"
-metaObjeto.log (metaObjeto.value)
-
-//fencao start
 const start = () => {
 
-    let count = 1
+    while (true) {
+        let opcao = "sair"
+        switch(opcao){
+            case "cadastrar": 
+                console.log("vamos cadastrar") 
+                break
 
-    while(count <= 10){
-        console.log(count++)
-    
+            case "listar":
+                console.log("vamos listar")
+                break
+
+            case "sair": console.log("encerrando...") 
+            return
+        }
     }
+
 }
 
 start()

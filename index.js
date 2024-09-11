187890b0fd481e02f2231f4c22b0884efdcf67c8
JohnnyBravo1644 <joavitorjuv@outlook.com>
2024-09-11 19:48:05 -0300

    criado o menu e a funcao cadastrarMetas

--- index.js
+++ index.js
@@ -1,10 +1,53 @@
-const start = () => {
+const { select, input, checkbox } = require('@inquirer/prompts')
 
+let meta = {
+    value: 'Tomar 3L de água por dia',
+    checked: false,
+}
+
+let metas = [meta];
+
+//funcao para cadastrar as metas
+const cadastrarMeta = async () => {
+    const meta = await input({message: "digite a meta:"})
+    
+    if(meta.length == 0) {
+        console.log("meta deve ser preenchida")
+        return
+    }
+    
+    metas.push({value: meta, checked: false})
+} 
+
+const listarMeta = async () => {
+    const resposta = await checkbox
+} 
+
+
+//funcao do menu
+const start = async () => {
+
+    
     while (true) {
-        let opcao = "sair"
+        
+        //funcao que mostra e cadastra as opcoes
+        const opcao = await select ({
+            message: "menu >",
+            choices: [
+                {name: "cadastrar meta", value: "cadastrar"},
+                {name: "sair", value: "sair"}
+            ]
+        })
+        
+        //switch case que possui as opcoes
         switch(opcao){
             case "cadastrar": 
-                console.log("vamos cadastrar") 
+                await cadastrarMeta()
+                console.log(metas)
+                break
+
+            case "cadastrar": 
+                await listarMeta()
                 break
 
             case "listar":
